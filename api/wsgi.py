fca2d234876a0e4e96e6f22d2c41db7ab6917f03
ahmadssadiq <91856553+ahmadssadiq@users.noreply.github.com>
2024-09-23 17:38:47 -0400

    Update vercel.json and adjust project structure

--- api/wsgi.py
+++ api/wsgi.py
@@ -12,7 +12,7 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-# Add the project directory to the Python path
+# Add the parent directory to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cs412.settings")
